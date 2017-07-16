a0e4d1849ba142cfe98d468f1d917827745d444b
choonkeun <choonkeun@yahoo.com>
2017-07-16 12:27:49 -0700

    Add files via upload

---
+++ WCFBasicService/EmployeeService.svc.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.IO;
+using Newtonsoft.Json;
+using DL_NorthWind;
+
+namespace WCFBasicService
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    public class EmployeeService : IEmployeeService
+    {
+        public List<Employee> GetEmployeeAll()
+        {
+            try
+            {
+                List<Employee> employee = EmployeeRepository.GetEmployeeAll();
+                if (employee == null)
+                {
+                    throw new FaultException("EMPLOYEE NOT FOUND");
+                }
+                return employee;
+            }
+            catch (Exception ex)
+            {
+                throw new FaultException(ex.Message + ex.StackTrace);
+            }
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            try
+            {
+                Employee employee = EmployeeRepository.GetEmployeeById(id.ToString());
+                if (employee == null)
+                {
+                    throw new FaultException("EMPLOYEE NOT FOUND");
+                }
+                return employee;
+            }
+            catch (Exception ex)
+            {
+                throw new FaultException(ex.Message + ex.StackTrace);
+            }
+        }
+
+        public Employee GetEmployeeByLastName(string lastName)
+        {
+            try
+            {
+                Employee employee = EmployeeRepository.GetEmployeeByName(lastName);
+                if (employee == null)
+                {
+                    throw new FaultException("EMPLOYEE NOT FOUND");
+                }
+                return employee;
+            }
+            catch (Exception ex)
+            {
+                throw new FaultException(ex.Message + ex.StackTrace);
+            }
+        }
+
+        public bool PutEmployee(Employee employee)
+        {
+            try
+            {
+                int stat = EmployeeRepository.PutEmployee(employee);
+                if (stat < 1)
+                {
+                    throw new FaultException("EMPLOYEE UPDATE FAILED");
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new FaultException(ex.Message + ex.StackTrace);
+            }
+        }
+        public bool PostEmployee(Stream streamData)
+        {
+            StreamReader reader = new StreamReader(streamData);
+            string res = reader.ReadToEnd();
+            reader.Close();
+            reader.Dispose();
+            Employee employee = JsonConvert.DeserializeObject<Employee>(res);
+            try
+            {
+                int stat = EmployeeRepository.PostEmployee(employee);
+                if (stat < 1)
+                {
+                    throw new FaultException("EMPLOYEE INSERT FAILED");
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new FaultException(ex.Message + ex.StackTrace);
+            }
+        }
+        public bool DeleteEmployee(int id)
+        {
+            try
+            {
+                Employee employee = GetEmployeeById(id);
+                if (employee == null)
+                {
+                    throw new FaultException("EMPLOYEE NOT FOUND");
+                }
+                int stat = EmployeeRepository.DeleteEmployee(id);
+                if (stat > 0) return true;
+            }
+            catch (Exception ex)
+            {
+                throw new FaultException(ex.Message + ex.StackTrace);
+            }
+            return false;
+        }
+    }
+
+}
---
+++ WCFBasicService/IEmployeeService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using DL_NorthWind;
+using System.IO;
+
+namespace WCFBasicService
+{
+    [ServiceContract]
+    //[ServiceContract(Namespace="http://localhost:39901/EmployeeService.svc")]
+    public interface IEmployeeService
+    {
+        // GET http://localhost:39901/EmployeeService.svc
+
+        [OperationContract]
+        // GET http://localhost:39901/EmployeeService.svc/GetEmployeeAll
+        [WebGet(UriTemplate = "/GetEmployeeAll", ResponseFormat = WebMessageFormat.Json)]
+        List<Employee> GetEmployeeAll();
+
+        [OperationContract]
+        // GET http://localhost:39901/EmployeeService.svc/GetEmployeeById/?id=1     
+        // ResponseFormat can be overrided by Accept: Application/json or Application/xml
+        [WebGet(UriTemplate = "/GetEmployeeById/?id={id}", ResponseFormat = WebMessageFormat.Json)]
+        Employee GetEmployeeById(int id);
+
+        [OperationContract]
+        // GET http://localhost:39901/EmployeeService.svc/GetEmployeeByLastName/Callahan
+        // ResponseFormat can be overrided by Accept: Application/json or Application/xml
+        [WebGet(UriTemplate = "/GetEmployeeByLastName/{lastName}", ResponseFormat = WebMessageFormat.Xml)]
+        Employee GetEmployeeByLastName(string lastName);
+
+        [OperationContract]
+        // PUT http://localhost:39901/EmployeeService.svc/PutEmployee
+        [WebInvoke(UriTemplate = "/PutEmployee/", Method = "PUT", BodyStyle = WebMessageBodyStyle.Bare, ResponseFormat = WebMessageFormat.Json)]
+        bool PutEmployee(Employee employee);
+
+        [OperationContract]
+        // POST http://localhost:39901/EmployeeService.svc/PostEmployee
+        [WebInvoke(UriTemplate = "/PostEmployee/", Method = "POST", BodyStyle = WebMessageBodyStyle.WrappedRequest, ResponseFormat = WebMessageFormat.Json)]
+        bool PostEmployee(Stream streamData);
+
+        [OperationContract]
+        // DELETE http://localhost:39901/EmployeeService.svc/DeleteEmployee
+        [WebInvoke(UriTemplate = "/DeleteEmployee/")]
+        bool DeleteEmployee(int id);
+    }
+
+}
